42a73a56c6cab93c750e11f66b80e161d5dfe458
Aries Wibowo <riespratama@gmail.com>
2022-02-17 06:36:45 +0700

    added adminLTE

---
+++ recordproduksi/app_settings.py
@@ -0,0 +1,3 @@
+from django.conf import settings
+
+ADMINLTE_URL = getattr(settings, 'ADMINLTE_URL')
--- recordproduksi/views.py
+++ recordproduksi/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView
+from app_settings import ADMINLTE_URL
 
-# Create your views here.
+from .models import RencanaProduksi, RencanaProduksiDetail
+
+class HompageView(ListView):
+    model = RencanaProduksi
+    template_name = 'index.html'
